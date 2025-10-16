6e75d189e0b83755c42cc77c17924381434e7c8f
Abdalla881 <osabdalla881@gmail.com>
2025-10-16 14:03:28 +0200

    upload vercel file2

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -33,6 +33,8 @@ async function bootstrap() {
   } else {
     app.use(morgan('combined'));
   }
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000){
+    console.log(`Application is running on: ${await app.getUrl()}`);
+  };
 }
 bootstrap();
